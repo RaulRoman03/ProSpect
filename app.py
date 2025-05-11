9bc547c313eb3805f439d31f6e3ffb4612a599c8
Raul Roman <raul.roman.125323@gmail.com>
2025-05-10 20:59:24 -0600

    app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='.')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/recruiter")
+def recruiter():
+    return render_template("recruiter.html")
+
+@app.route("/player")
+def player():
+    return render_template("player.html")
+
+@app.route("/coach")
+def coach():
+    return render_template("coach.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
